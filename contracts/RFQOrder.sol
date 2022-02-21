d9ae4af4b9fdcc9bb090ea9822142f21c23681dd
LuckyMark01 <markharris.sanf01@outlook.com>
2022-02-21 11:07:25 +0900

    edited readme.md & RFQorder.sol added

---
+++ contracts/RFQOrder.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+abstract contract RFQOrder is EIP712 {
+    using SafeERC20 for IERC20;
+
+    event RFQOrderFilled(
+        bytes32 orderHash,
+        uint256 makingAmount
+    );
+
+    struct OrderRFQ {
+        uint256 info;  // lowest 64 bits is the order id, next 64 bits is the expiration timestamp
+        IERC20 makerAsset;
+        IERC20 takerAsset;
+        address maker;
+        address allowedSender;  // equals to Zero address on public orders
+        uint256 makingAmount;
+        uint256 takingAmount;
+    }
+
+    bytes32 constant public LIMIT_ORDER_RFQ_TYPEHASH = keccak256(
+        "OrderRFQ(uint256 info,address makerAsset,address takerAsset,address maker,address allowedSender,uint256 makingAmount,uint256 takingAmount)"
+    );
+
+}
