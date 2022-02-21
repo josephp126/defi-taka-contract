e339f63c4e8700f59e6559e441d58a23601570e0
LuckyMark01 <markharris.sanf01@outlook.com>
2022-02-21 12:39:54 +0900

    added feature for canceling orderRFQ

--- contracts/RFQOrder.sol
+++ contracts/RFQOrder.sol
@@ -6,26 +6,48 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/// @title RFQ Limit Order
 abstract contract RFQOrder is EIP712 {
     using SafeERC20 for IERC20;
-
-    event RFQOrderFilled(
-        bytes32 orderHash,
-        uint256 makingAmount
-    );
+    
+    /// @notice Emitted when RFQ gets filled
+    event OrderFilledRFQ(bytes32 orderHash, uint256 makingAmount);
 
     struct OrderRFQ {
-        uint256 info;  // lowest 64 bits is the order id, next 64 bits is the expiration timestamp
+        uint256 info; // lowest 64 bits is the order id, next 64 bits is the expiration timestamp
         IERC20 makerAsset;
         IERC20 takerAsset;
         address maker;
-        address allowedSender;  // equals to Zero address on public orders
+        address allowedSender; // equals to Zero address on public orders
         uint256 makingAmount;
         uint256 takingAmount;
     }
 
-    bytes32 constant public LIMIT_ORDER_RFQ_TYPEHASH = keccak256(
-        "OrderRFQ(uint256 info,address makerAsset,address takerAsset,address maker,address allowedSender,uint256 makingAmount,uint256 takingAmount)"
+    bytes32 public constant LIMIT_ORDER_RFQ_TYPEHASH = keccak256(
+            "OrderRFQ(uint256 info,address makerAsset,address takerAsset,address maker,address allowedSender,uint256 makingAmount,uint256 takingAmount)"
     );
 
+    mapping(address => mapping(uint256 => uint256)) private _invalidator;
+
+    /// @notice Returns bitmask for double-spend invalidators based on lowest byte of order.info and filled quotes
+    /// @return Result Each bit represents whether corresponding was already invalidated
+    function _invalidatorForOrderRFQ(address maker, uint256 slot) external view returns (uint256) {
+        return _invalidator[maker][slot];
+    }
+
+    /// @notice Cancels order's quote
+    function cancelOrderRFQ(uint256 orderInfo) external {
+        _invalidateOrder(msg.sender, orderInfo);
+    }
+
+    function _invalidateOrder(address maker, uint256 orderInfo) private {
+        uint256 invalidatorSlot = uint64(orderInfo) >> 8;
+        uint256 invalidatorBit = 1 << uint8(orderInfo);
+        mapping(uint256 => uint256) storage invalidatorStorage = _invalidator[
+            maker
+        ];
+        uint256 invalidator = invalidatorStorage[invalidatorSlot];
+        require(invalidator & invalidatorBit == 0, "LOP: invalidator order");
+        invalidatorStorage[invalidatorSlot] = invalidator | invalidatorBit;
+    }
 }
