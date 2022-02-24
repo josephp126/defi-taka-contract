531fc21c630088704ae8688b82b185698bb7bb3d
LuckyMark01 <markharris.sanf01@outlook.com>
2022-02-24 12:19:04 +0900

    added helpers/utils.ts

---
+++ test/helpers/utils.ts
@@ -0,0 +1,7 @@
+import { BN, ether } from '@openzeppelin/test-helpers';
+
+const addr1PrivateKey = 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+
+export function toBN (num) {
+    return new BN(num);
+}
