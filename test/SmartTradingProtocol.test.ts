609c50ea929485a37a4f4858bb44a3b9dd1e6866
LuckyMark01 <markharris.sanf01@outlook.com>
2022-02-24 11:26:08 +0900

    added helpers/eip712.ts

--- test/SmartTradingProtocol.test.ts
+++ test/SmartTradingProtocol.test.ts
@@ -1,12 +1,40 @@
-const { expectRevert, BN, time, constants } = require('@openzeppelin/test-helpers');
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { expectRevert, BN, time, constants } from '@openzeppelin/test-helpers';
+import { expect } from "chai";
+import { ethers } from "hardhat";
 
-const { bufferToHex } = require('ethereumjs-util');
-const ethSigUtil = require('eth-sig-util');
+import { bufferToHex } from 'ethereumjs-util';
+import ethSigUtil from 'eth-sig-util';
 const Wallet = require('ethereumjs-wallet').default;
 
 describe("SmartTradingProtocol", async function () {
-    let addr:any, wallet: any;
+    let addr1:any, wallet: any;
+
+    function buildOrderRFQ (info: any, makerAsset: any, takerAsset: any, makingAmount: any, takingAmount: any, allowedSender = constants.ZERO_ADDRESS) {
+        return {
+            info,
+            makerAsset: makerAsset.address,
+            takerAsset: takerAsset.address,
+            maker: wallet,
+            allowedSender,
+            makingAmount,
+            takingAmount,
+        };
+    }
+    describe('OrderRFQ Cancelation', async function () {
+        it('should cancel own order', async function () {
+            await this.swap.cancelOrderRFQ('1');
+            const invalidator = await this.swap.invalidatorForOrderRFQ(addr1, '0');
+            expect(invalidator).to.be.bignumber.equal(toBN('2'));
+        });
+
+        it('should cancel own order with huge number', async function () {
+            await this.swap.cancelOrderRFQ('1023');
+            const invalidator = await this.swap.invalidatorForOrderRFQ(addr1, '3');
+            expect(invalidator).to.be.bignumber.equal(toBN('1').shln(255));
+        });
+
+       
+    });
+
 
 })
---
+++ test/helpers/eip712.ts
@@ -0,0 +1,38 @@
+import { Address } from "cluster";
+
+// import { BN } from '@openzeppelin/test-helpers';
+import ethSigUtil from 'eth-sig-util';
+// import { fromRpcSig } from 'ethereumjs-util';
+
+const EIP712Domain = [
+    { name: 'name', type: 'string' },
+    { name: 'version', type: 'string' },
+    { name: 'chainId', type: 'uint256' },
+    { name: 'verifyingContract', type: 'address' },
+];
+
+const Permit = [
+    { name: 'owner', type: 'address' },
+    { name: 'spender', type: 'address' },
+    { name: 'value', type: 'uint256' },
+    { name: 'nonce', type: 'uint256' },
+    { name: 'deadline', type: 'uint256' },
+];
+
+function domainSeparator (name: String, version: String, chainId: any, verifyingContract: Address) {
+    return '0x' + ethSigUtil.TypedDataUtils.hashStruct(
+        'EIP712Domain',
+        { name, version, chainId, verifyingContract },
+        { EIP712Domain },
+    ).toString('hex');
+}
+
+function buildData (owner: Address, name: String, version: String, chainId: any, verifyingContract: Address, spender: Address, nonce: any, value: any, deadline: any) {
+    return {
+        primaryType: 'Permit',
+        types: { EIP712Domain, Permit },
+        domain: { name, version, chainId, verifyingContract },
+        message: { owner, spender, value, nonce, deadline },
+    };
+}
+
