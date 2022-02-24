06b7f5c7fe3a2f03cba6c020c09a4b4f0a96f9a5
LuckyMark01 <markharris.sanf01@outlook.com>
2022-02-24 10:39:58 +0900

    modified and renamed sample.js to deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const TakaDAO = await hre.ethers.getContractFactory("SmartTradingProtocol");
+  const takadao = await TakaDAO.deploy();
 
-  await greeter.deployed();
+  await takadao.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("TakaDAO deployed to:", takadao.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/SmartTradingProtocol.test.ts
+++ test/SmartTradingProtocol.test.ts
@@ -2,6 +2,10 @@ const { expectRevert, BN, time, constants } = require('@openzeppelin/test-helper
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const { bufferToHex } = require('ethereumjs-util');
+const ethSigUtil = require('eth-sig-util');
+const Wallet = require('ethereumjs-wallet').default;
+
 describe("SmartTradingProtocol", async function () {
     let addr:any, wallet: any;
 
