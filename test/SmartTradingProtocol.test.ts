7eebd690f379d1fecbab4fe6e1f66352c2c9b985
LuckyMark01 <markharris.sanf01@outlook.com>
2022-02-24 10:15:42 +0900

    added SmartTradingProtocol.test.ts

---
+++ test/SmartTradingProtocol.test.ts
@@ -0,0 +1,8 @@
+const { expectRevert, BN, time, constants } = require('@openzeppelin/test-helpers');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("SmartTradingProtocol", async function () {
+    let addr:any, wallet: any;
+
+})
