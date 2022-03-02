52713bbad9622e9110e4839342d6c4bb86d90ac4
LuckyMark01 <markharris.sanf01@outlook.com>
2022-03-02 16:13:58 +0900

    implemented cancelOrder method

--- contracts/LimitOrder.sol
+++ contracts/LimitOrder.sol
@@ -9,12 +9,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./helpers/AmountCalculator.sol";
 import "./libraries/Permitable.sol";
 
-/// @title Regular Limit Order 
-abstract contract LimitOrder is
-    EIP712,
-    AmountCalculator,
-    Permitable
-{
+/// @title Regular Limit Order
+abstract contract LimitOrder is EIP712, AmountCalculator, Permitable {
     using Address for address;
 
     /// @notice Emitted every time order gets filled, including partial fills
@@ -25,7 +21,7 @@ abstract contract LimitOrder is
     );
 
     /// @notice Emitted when order gets cancelled
-    event OrderCancelled(
+    event OrderCanceled(
         address indexed maker,
         bytes32 orderHash,
         uint256 remainingRaw
@@ -38,7 +34,7 @@ abstract contract LimitOrder is
         address takerAsset;
         address maker;
         address receiver;
-        address allowedSender;  // equals to Zero address on public orders
+        address allowedSender; // equals to Zero address on public orders
         uint256 makingAmount;
         uint256 takingAmount;
     }
@@ -50,30 +46,30 @@ abstract contract LimitOrder is
         address takerAsset;
         address maker;
         address receiver;
-        address allowedSender;  // equals to Zero address on public orders
+        address allowedSender; // equals to Zero address on public orders
         uint256 makingAmount;
         uint256 takingAmount;
         bytes makerAssetData;
         bytes takerAssetData;
         bytes getMakerAmount; // this.staticcall(abi.encodePacked(bytes, swapTakerAmount)) => (swapMakerAmount)
         bytes getTakerAmount; // this.staticcall(abi.encodePacked(bytes, swapMakerAmount)) => (swapTakerAmount)
-        bytes predicate;      // this.staticcall(bytes) => (bool)
-        bytes permit;         // On first fill: permit.1.call(abi.encodePacked(permit.selector, permit.2))
+        bytes predicate; // this.staticcall(bytes) => (bool)
+        bytes permit; // On first fill: permit.1.call(abi.encodePacked(permit.selector, permit.2))
         bytes interaction;
     }
 
-    bytes32 constant public LIMIT_ORDER_TYPEHASH = keccak256(
+    bytes32 public constant LIMIT_ORDER_TYPEHASH = keccak256(
         "Order(uint256 salt,address makerAsset,address takerAsset,address maker,address receiver,address allowedSender,uint256 makingAmount,uint256 takingAmount,bytes makerAssetData,bytes takerAssetData,bytes getMakerAmount,bytes getTakerAmount,bytes predicate,bytes permit,bytes interaction)"
     );
-    uint256 constant private _ORDER_DOES_NOT_EXIST = 0;
-    uint256 constant private _ORDER_FILLED = 1;
+    uint256 private constant _ORDER_DOES_NOT_EXIST = 0;
+    uint256 private constant _ORDER_FILLED = 1;
 
     /// @notice Stores unfilled amounts for each order plus one.
     /// Therefore 0 means order doesn't exist and 1 means order was filled
     mapping(bytes32 => uint256) private _remaining;
 
     /// @notice Returns unfilled amount for order. Throws if order does not exist
-    function remaining(bytes32 orderHash) external view returns(uint256) {
+    function remaining(bytes32 orderHash) external view returns (uint256) {
         uint256 amount = _remaining[orderHash];
         require(amount != _ORDER_DOES_NOT_EXIST, "LOP: Unknown order");
         unchecked { amount -= 1; }
@@ -82,7 +78,51 @@ abstract contract LimitOrder is
 
     /// @notice Returns unfilled amount for order
     /// @return Result Unfilled amount of order plus one if order exists. Otherwise 0
-    function remainingRaw(bytes32 orderHash) external view returns(uint256) {
+    function remainingRaw(bytes32 orderHash) external view returns (uint256) {
         return _remaining[orderHash];
+    }
+
+    /// @notice Same as `remainingRaw` but for multiple orders
+    function remainingsRaw(bytes32[] memory orderHashes) external view returns (uint256[] memory)
+    {
+        uint256[] memory results = new uint256[](orderHashes.length);
+        for (uint256 i = 0; i < orderHashes.length; i++) {
+            results[i] = _remaining[orderHashes[i]];
+        }
+        return results;
+    }
+
+    /// @notice Cancels order by setting remaining amount to zero
+    function cancelOrder(Order memory order) external {
+        require(order.maker == msg.sender, "LOP: Access denied");
+
+        bytes32 orderHash = hashOrder(order);
+        uint256 orderRemaining = _remaining[orderHash];
+        require(orderRemaining != _ORDER_FILLED, "LOP: already filled");
+        emit OrderCanceled(msg.sender, orderHash, orderRemaining);
+        _remaining[orderHash] = _ORDER_FILLED;
+    }
+
+    function hashOrder(Order memory order) public view returns (bytes32) {
+        StaticOrder memory staticOrder;
+        assembly {
+            // solhint-disable-line no-inline-assembly
+            staticOrder := order
+        }
+        return _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    LIMIT_ORDER_TYPEHASH,
+                    staticOrder,
+                    keccak256(order.makerAssetData),
+                    keccak256(order.takerAssetData),
+                    keccak256(order.getMakerAmount),
+                    keccak256(order.getTakerAmount),
+                    keccak256(order.predicate),
+                    keccak256(order.permit),
+                    keccak256(order.interaction)
+                )
+            )
+        );
     }
 }
