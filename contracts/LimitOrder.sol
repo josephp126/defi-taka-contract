b446b91fa895675b4473b79856210f05a640dcf0
LuckyMark01 <markharris.sanf01@outlook.com>
2022-03-02 12:17:22 +0900

    defined static order & extented common order structure

--- contracts/LimitOrder.sol
+++ contracts/LimitOrder.sol
@@ -31,6 +31,43 @@ abstract contract LimitOrder is
         uint256 remainingRaw
     );
 
-    
+    // Fixed-size order part with core information
+    struct StaticOrder {
+        uint256 salt;
+        address makerAsset;
+        address takerAsset;
+        address maker;
+        address receiver;
+        address allowedSender;  // equals to Zero address on public orders
+        uint256 makingAmount;
+        uint256 takingAmount;
+    }
+
+    // `StaticOrder` extension including variable-sized additional order meta information
+    struct Order {
+        uint256 salt;
+        address makerAsset;
+        address takerAsset;
+        address maker;
+        address receiver;
+        address allowedSender;  // equals to Zero address on public orders
+        uint256 makingAmount;
+        uint256 takingAmount;
+        bytes makerAssetData;
+        bytes takerAssetData;
+        bytes getMakerAmount; // this.staticcall(abi.encodePacked(bytes, swapTakerAmount)) => (swapMakerAmount)
+        bytes getTakerAmount; // this.staticcall(abi.encodePacked(bytes, swapMakerAmount)) => (swapTakerAmount)
+        bytes predicate;      // this.staticcall(bytes) => (bool)
+        bytes permit;         // On first fill: permit.1.call(abi.encodePacked(permit.selector, permit.2))
+        bytes interaction;
+    }
+
+    bytes32 constant public LIMIT_ORDER_TYPEHASH = keccak256(
+        "Order(uint256 salt,address makerAsset,address takerAsset,address maker,address receiver,address allowedSender,uint256 makingAmount,uint256 takingAmount,bytes makerAssetData,bytes takerAssetData,bytes getMakerAmount,bytes getTakerAmount,bytes predicate,bytes permit,bytes interaction)"
+    );
+    uint256 constant private _ORDER_DOES_NOT_EXIST = 0;
+    uint256 constant private _ORDER_FILLED = 1;
+
+
 
 }
